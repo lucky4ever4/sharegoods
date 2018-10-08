f3c2a1fde4d1510a66d05317766bc430d40ae087
Ion HAREA <vaniusha.harea.1997@mail.ru>
2018-10-08 14:41:34 +0300

    new_master

---
+++ src/main/java/com/sharegoods/inth3rship/Inth3rshipApplication.java
@@ -0,0 +1,12 @@
+package com.sharegoods.inth3rship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inth3rshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inth3rshipApplication.class, args);
+	}
+}
