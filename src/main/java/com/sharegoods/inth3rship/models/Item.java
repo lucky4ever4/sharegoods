74965f0265f1c25594c11bff14053766fa541158
Oxana Dunav <oxana.dunav@gmail.com>
2018-10-10 16:34:39 +0300

    implement Item, ItemRepository (#3)

--- src/main/java/com/sharegoods/inth3rship/models/Item.java
+++ src/main/java/com/sharegoods/inth3rship/models/Item.java
@@ -1,4 +1,69 @@
 package com.sharegoods.inth3rship.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Date dateTime;
+    private String title;
+    private String description;
+
+    public Item() {
+    }
+
+    public Item(Long userId, Date dateTime, String title, String description) {
+        this.userId = userId;
+        this.dateTime = dateTime;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
