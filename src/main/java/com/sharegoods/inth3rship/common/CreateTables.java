e0389b6d832c623886f42dfbda22df40bb9da443
Oxana Dunav <oxana.dunav@gmail.com>
2018-10-10 16:34:39 +0300

    implement Item, ItemRepository (#3)

--- src/main/java/com/sharegoods/inth3rship/common/CreateTables.java
+++ src/main/java/com/sharegoods/inth3rship/common/CreateTables.java
@@ -1,10 +1,14 @@
 package com.sharegoods.inth3rship.common;
 
+import com.sharegoods.inth3rship.models.Item;
 import com.sharegoods.inth3rship.models.User;
+import com.sharegoods.inth3rship.repositories.ItemRepository;
 import com.sharegoods.inth3rship.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.Date;
 
 @Configuration
 public class CreateTables
@@ -15,6 +19,14 @@ public class CreateTables
         return args -> {
             repository.save(new User("Oxana","Lastname","oxana@gmail.com","123"));
             repository.save(new User("Ion","Lastname","ion@gmail.com","asjashdl"));
+        };
+    }
+
+    @Bean
+    CommandLineRunner itemsSampleData(ItemRepository repository)
+    {
+        return args -> {
+            repository.save(new Item(1L, Date.valueOf("2018-10-10"), "myTitle", "laptop"));
         };
     }
 }
--- src/main/java/com/sharegoods/inth3rship/repositories/ItemRepository.java
+++ src/main/java/com/sharegoods/inth3rship/repositories/ItemRepository.java
@@ -1,4 +1,9 @@
 package com.sharegoods.inth3rship.repositories;
 
-public class ItemRepository {
+import com.sharegoods.inth3rship.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemRepository extends JpaRepository<Item, Long> {
 }
