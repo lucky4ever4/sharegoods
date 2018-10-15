556c0292c5e6dc7d13dbfd5527968afdb02cf317
alex <alex.c.turbo@gmail.com>
2018-10-15 10:56:28 +0300

    Added exception handling to all methods in ItemController, returning custom codes and messages; also added Dto to responses

--- src/main/java/com/sharegoods/inth3rship/dto/ItemDto.java
+++ src/main/java/com/sharegoods/inth3rship/dto/ItemDto.java
@@ -3,19 +3,35 @@ package com.sharegoods.inth3rship.dto;
 import com.sharegoods.inth3rship.models.Item;
 import com.sharegoods.inth3rship.models.User;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+
 public class ItemDto {
     Long id;
     Long userId;
     String title;
     String description;
 
-    public ItemDto() {}
+    public ItemDto() {
+    }
 
     public ItemDto(Item item) {
         this.id = item.getId();
         this.title = item.getTitle();
         this.description = item.getDescription();
         this.userId = item.getUser().getId();
+    }
+
+    public static List<ItemDto> getItemDtoList(List<Item> itemList) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        ListIterator<Item> itemsIterator = itemList.listIterator();
+        while (itemsIterator.hasNext()) {
+            ItemDto itemDto = new ItemDto(itemsIterator.next());
+            itemDtoList.add(itemDto);
+        }
+        return itemDtoList;
     }
 
     public Long getId() {
--- src/main/java/com/sharegoods/inth3rship/restcontrollers/ItemController.java
+++ src/main/java/com/sharegoods/inth3rship/restcontrollers/ItemController.java
@@ -21,46 +21,73 @@ public class ItemController {
         this.itemService = itemService;
     }
 
-    @GetMapping("/users/{id}/items")
-    public List<Item> getItemsByUserId(@PathVariable("id") Long id) {
-        return itemService.getItemsByUserId(id);
+    @GetMapping("/users/{userId}/items")
+    public ResponseEntity getItemsByUserId(@PathVariable("userId") Long userId) {
+        try {
+            List<Item> itemList = itemService.getItemsByUserId(userId);
+
+            if (itemList.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).body("User has not items");
+            }
+
+            List<ItemDto> itemDtoList = ItemDto.getItemDtoList(itemList);
+            return ResponseEntity.status(HttpStatus.OK).body(itemDtoList);
+
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
     }
 
     @GetMapping("/items")
-    public List<Item> getItems() {
-        return itemService.getItems();
+    public ResponseEntity getItems() {
+        List<Item> itemList = itemService.getItems();
+
+        if (itemList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT).body("No items found");
+        }
+        List<ItemDto> itemDtoList = ItemDto.getItemDtoList(itemList);
+        return ResponseEntity.status(HttpStatus.OK).body(itemDtoList);
     }
 
     @PostMapping("/users/{id}/items")
-    public ResponseEntity<ItemDto> createItem(@PathVariable("id") Long userId,
+    public ResponseEntity createItem(@PathVariable("id") Long userId,
                                      @RequestParam("title") String title,
                                      @RequestParam("description") String description) {
         Item item = itemService.createNewItem(userId, title, description);
-        return new ResponseEntity<>(new ItemDto(item), HttpStatus.OK);
+        ItemDto itemDto = new ItemDto(item);
+    return ResponseEntity.status(HttpStatus.OK).body(itemDto);
     }
 
     @GetMapping("/items/{id}")
     public ResponseEntity getItemById(@PathVariable("id") Long id) {
-        try
-        {
+        try {
             Item item = itemService.getItemById(id);
             return ResponseEntity.status(HttpStatus.OK).body(new ItemDto(item));
-        }
-        catch (NoSuchElementException e)
-        {
+        } catch (NoSuchElementException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item not found");
         }
     }
 
     @DeleteMapping("/items/{id}")
-    public void deleteItem(@PathVariable("id") Long id) {
-        itemService.deleteItem(id);
+    public ResponseEntity deleteItem(@PathVariable("id") Long id) {
+        try {
+            itemService.deleteItem(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Deleted successfully");
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item not found");
+        }
     }
 
-    @PutMapping("/users/{userId}/items/{itemId}")
-    public Item updateItem(@PathVariable("itemId") Long itemId,
-                           @RequestParam("title") String title,
-                           @RequestParam("description") String description) {
-        return itemService.updateItem(itemId, title, description);
+    @PutMapping("/items/{itemId}")
+    public ResponseEntity updateItem(@PathVariable("itemId") Long itemId,
+                                     @RequestParam("title") String title,
+                                     @RequestParam("description") String description) {
+        try {
+            Item item = itemService.updateItem(itemId, title, description);
+            ItemDto itemDto = new ItemDto(item);
+            return ResponseEntity.status(HttpStatus.OK).body(itemDto);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item not found");
+        }
     }
 }
